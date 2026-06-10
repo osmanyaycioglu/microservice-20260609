da8cd2543f8429c5b47d105e90b6e9756faf9d7b
osman yaycioglu <osman.yaycioglu@gmail.com>
2026-06-10 14:32:32 +0300

    second 3

--- ms-order/src/main/java/org/training/microservice/msorder/MsOrderApplication.java
+++ ms-order/src/main/java/org/training/microservice/msorder/MsOrderApplication.java
@@ -2,13 +2,22 @@ package org.training.microservice.msorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class MsOrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MsOrderApplication.class,
                               args);
     }
+
+    @Scheduled(fixedDelay = 10_000)
+    public void method(){
+        System.out.println("Hello");
+    }
+
 
 }
